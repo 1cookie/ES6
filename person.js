80e210b186b9f592a56c02fad3d0ff73a89e5723
El Capitan <1cookie@users.noreply.github.com>
2021-06-07 14:11:29 +0100

    Update person.js

--- person.js
+++ person.js
@@ -21,3 +21,9 @@ class Person extends Human {
 const person = new Person();
 person.printMyName();
 person.printGender();
+
+
+// Spread...
+const numbers = [1,2,3];
+const newNumbers = [...numbers, 4,5];
+console.log(newNumbers);
